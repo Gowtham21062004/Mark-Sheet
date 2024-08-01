a80d430edf3a4efec21eca8d3017ab39ca7f28a2
Gowtham21062004 <142236401+Gowtham21062004@users.noreply.github.com>
2024-08-01 22:27:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function add1(){
+    const val1=document.getElementById("val1").value;
+    const val2=document.getElementById("val2").value;
+    const res1=document.getElementById("res1");
+    res1.innerHTML=Number(val1)+Number(val2);
+}
+function add2(){
+    const val3=document.getElementById("val3").value;
+    const val4=document.getElementById("val4").value;
+    const res2=document.getElementById("res2");
+    res2.innerHTML=Number(val3)+Number(val4);
+}
+function add3(){
+    const val5=document.getElementById("val5").value;
+    const val6=document.getElementById("val6").value;
+    const res3=document.getElementById("res3");
+    res3.innerHTML=Number(val5)+Number(val6);
+}
+function add4(){
+    const val7=document.getElementById("val7").value;
+    const val8=document.getElementById("val8").value;
+    const res4=document.getElementById("res4");
+    res4.innerHTML=Number(val7)+Number(val8);
+}
+function add5(){
+    const val9=document.getElementById("val9").value;
+    const val10=document.getElementById("val10").value;
+    const res5=document.getElementById("res5");
+    res5.innerHTML=Number(val9)+Number(val10);
+}
+function add6(){
+    const val11=document.getElementById("val11").value;
+    const val12=document.getElementById("val12").value;
+    const res6=document.getElementById("res6");
+    res6.innerHTML=Number(val11)+Number(val12);
+}
